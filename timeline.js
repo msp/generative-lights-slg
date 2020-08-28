3a6a6fe482cc8a41525594233170015cc0bf915b
msp <matt@cenatus.org>
2020-08-28 14:09:55 +0100

    Update timeline for Metro Arts
    This is copied from the live Insomnia config

--- timeline.js
+++ timeline.js
@@ -10,13 +10,13 @@ var set2Light2 = new timelineUtils.Timeline('set2Light2', 4);
 var set2Projector1 = new timelineUtils.Timeline('set2Projector1', 5);
 
 //post("MSP JSON set1Light1: "+JSON.stringify(set1Light1)+ "\n");
-set1Light1.events = [1000, 3000, 5000];
-set1Light2.events = [100, 1500, 3500, 5500];
-set1Projector1.events = [50, 1001, 2500];
+set1Light1.events = [100, 3000, 5500];
+set1Light2.events = [100, 2500, 5500];
+set1Projector1.events = [50, 2500, 7800];
 
-set2Light1.events = [1000, 3000, 5000];
-set2Light2.events = [100, 1500, 3500, 5500];
-set2Projector1.events = [50, 1001, 2500];
+set2Light1.events = [1000, 2500, 5000];
+set2Light2.events = [300, 2000, 5000, 9000];
+set2Projector1.events = [200, 3000, 8000];
 
 // set1Light1.events = [100, 500];
 // set1Light2.events = [300];
