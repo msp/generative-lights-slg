21224078bccc6b1dd9a9afb2d5815c17d0088bac
msp <matt@cenatus.org>
2020-08-31 11:46:07 +0100

    Timeline for Metro

--- timeline.js
+++ timeline.js
@@ -8,24 +8,15 @@ var set1Projector1 = new timelineUtils.Timeline('set1Projector1', 2);
 var set2Light1 = new timelineUtils.Timeline('set2Light1', 3);
 var set2Light2 = new timelineUtils.Timeline('set2Light2', 4);
 var set2Projector1 = new timelineUtils.Timeline('set2Projector1', 5);
-
 //post("MSP JSON set1Light1: "+JSON.stringify(set1Light1)+ "\n");
-// set1Light1.events = [100, 3000, 5500];
-// set1Light2.events = [100, 2500, 5500];
-// set1Projector1.events = [50, 2500, 7800];
 
 set1Light1.events = [100, 3000, 5500];
-set1Light2.events = [100, 2500, 5500];
-set1Projector1.events = [50, 2500, 7800];
+set1Light2.events = [100, 800, 3050];
+set1Projector1.events = [500, 4500, 9000];
 
-set2Light1.events = [1000, 2500, 5000];
-set2Light2.events = [300, 2000, 5000, 9000];
-set2Projector1.events = [200, 3000, 8000];
-
-
-// set2Light1.events = [1000, 1500];
-// set2Light2.events = [1300];
-// set2Projector1.events = [100, 1000];
+set2Light1.events = [500, 2000, 6000];
+set2Light2.events = [300, 1200, 4000];
+set2Projector1.events = [800, 6000, 10000];
 
 inlets = 2;
 outlets = 6;
